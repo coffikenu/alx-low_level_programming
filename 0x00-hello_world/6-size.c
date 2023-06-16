1645eefc636c086adb5b606530ada9a636b43108
coffikenu <kenisatetemke@gmail.com>
2023-06-16 05:50:26 -0700

    after along run

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %ld byte\n",sizeof(char));
+printf("Size of a int: %ld byte\n",sizeof(int));
+printf("size of a long int, %ld byte\n",sizeof(long int));
+printf("Size of a long long int, %ld byte\n",sizeof(long long int));
+printf("Size of a float, %ld byte\n",sizeof(float));
+}

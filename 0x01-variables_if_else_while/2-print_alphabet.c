251b12606a64e11a3969052b6dc931fb98ca8e07
coffikenu <kenisatetemke@gmail.com>
2023-06-17 01:24:35 -0700

    0

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
+ int i = 0;
+ while(i<26)
+{
+putchar(alphabet[i]);
+if (i == 25)
+{
+putchar('\n');
+}
+i++;
+}
+return (0);
+}

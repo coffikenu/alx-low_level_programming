631651cf14ee5d28c559a76bac45ed0f8c49b701
coffikenu <kenisatetemke@gmail.com>
2023-06-17 10:59:48 -0700

    yes i got it

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int i = 0;
 while (i < 9)
 {
-int j = i+1;
+int j = i + 1;
 while (j < 10)
 {
 putchar(i + '0');
@@ -17,15 +19,13 @@ if (i < 8)
 putchar(',');
 putchar(' ');
 }
- j++;
+j++;
 }
 if (i == 8)
 {
 putchar('\n');
 }
- i++;
-
-
+i++;
 } 
 return (0);
 }

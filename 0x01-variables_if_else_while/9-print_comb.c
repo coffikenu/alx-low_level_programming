90aa93ada017adf257a3b9586e61d1d2714e0ecd
coffikenu <kenisatetemke@gmail.com>
2023-06-17 05:17:59 -0700

    rodrich

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 0;
+while (i < 10)
+{
+putchar(i + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+if (i == 9)
+{
+putchar('\n');
+}
+i++;
+}
+return (0);
+}

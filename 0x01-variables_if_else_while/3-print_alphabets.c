043265bcf48825b0600c50aae5bbce368fdabfbb
coffikenu <kenisatetemke@gmail.com>
2023-06-17 01:44:44 -0700

    yes

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**                                                                                                                                                  
+ * main - Entry point                                                                                                                                
+ *                                                                                                                                                   
+ * Return: Always 0 (Success)                                                                                                                        
+ */
+int main(void)
+{
+char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int i = 0;
+while (i < 52)
+{
+putchar(alphabet[i]);
+if (i == 51)
+{
+putchar('\n');
+}
+i++;
+}
+return (0);
+}
+

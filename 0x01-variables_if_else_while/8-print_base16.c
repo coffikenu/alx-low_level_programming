0c477f181999acde8e286d754f77942bf2fb8eb1
coffikenu <kenisatetemke@gmail.com>
2023-06-17 03:36:29 -0700

    not easy

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success) 
+ */
+int main(void)
+{
+char alpha[16] = "0123456789abcdef";
+int i = 0;
+while (i < 16)
+{
+putchar(alpha[i]);
+if (i == 15)
+{
+putchar('\n');
+}
+i++;
+}
+return (0);
+}

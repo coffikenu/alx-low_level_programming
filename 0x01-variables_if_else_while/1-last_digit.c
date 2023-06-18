8e37d6c2c2e15477244511d3d7d7dcbac77d4033
coffikenu <kenisatetemke@gmail.com>
2023-06-18 06:04:41 -0700

    kenisa

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,15 @@ st = s[strlen(s)-1];
 sscanf(&st, "%d", &num);
 if ( n > 0 && num > 5)
 {
-printf("last digit of  %d is %d and is greater than 5\n",  n,num);
+printf("last digit of %d is %d and is greater than 5\n",  n,num);
 }
 else if ( num != 0 && n < 6 && n < 6)
 {
-printf("last digit of  %d is %d and is less than 6 and not 0\n", n,num);
+printf("last digit of %d is %d and is less than 6 and not 0\n", n,num);
 }
 else if (num == 0)
 {
-printf("last digit of  %d is %d is 0\n", n,num);
+printf("last digit of %d is %d and is 0\n", n,num);
 }
 return (0);
 }

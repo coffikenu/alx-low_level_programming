87ebc94b5c0eff6d3bbf7cd6b13c63262b2fd8be
coffikenu <kenisatetemke@gmail.com>
2023-06-18 06:12:29 -0700

    kenisa

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -26,7 +26,7 @@ printf("Last digit of %d is %d and is greater than 5\n",  n,num);
 }
 else if ( num != 0 && n < 6 && n < 6)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n,num);
+printf("Last digit of %d is -%d and is less than 6 and not 0\n", n,num);
 }
 else if (num == 0)
 {

efb86cd36ea9fc0ea51b448a061833613b2d2636
coffikenu <kenisatetemke@gmail.com>
2023-06-23 08:13:50 -0700

    lost my way

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - check the code
+ * @size: is parametr
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+int i;
+int j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+putchar('#');
+if (j == size-1)
+{
+putchar('\n');
+}
+}
+}
+if (size <=0)
+{
+putchar('\n');
+}
+}

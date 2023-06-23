65b6e282a81cb2a35b04107fa42c4e16c52cd0a3
coffikenu <kenisatetemke@gmail.com>
2023-06-23 08:49:07 -0700

    loding

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for(i = 0; i < 101; i++)
+{
+if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if(i % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else
+{
+printf("%d ", i);
+}
+}
+if (i >= 100)
+{
+  printf("\n");
+}
+return (0);
+}

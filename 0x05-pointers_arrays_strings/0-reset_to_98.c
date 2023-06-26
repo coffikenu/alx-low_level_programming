3552086f5962c892657f2bf9fb908161e5dca65b
coffikenu <kenisatetemke@gmail.com>
2023-06-26 08:39:12 -0700

    fetariye erdagn

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98 - check the code 
- *@n - is the parametr
+ * reset_to_98 - check the code
+ * @n - is the parametr
  * Return: Always 0.
  */
 void reset_to_98(int *n)

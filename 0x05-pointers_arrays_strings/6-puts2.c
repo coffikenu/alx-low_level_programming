d53e42162ca56f3435737150e84d913aa29e5957
coffikenu <kenisatetemke@gmail.com>
2023-06-27 03:02:50 -0700

    lost

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - check the code
+ * @str: is a parametr
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+int i;
+int b = strlen(str);
+for (i = 0; i <= b-1; i++)
+{
+if(i % 2 == 0)
+{
+putchar(str[i]);
+}
+}
+putchar('\n');
+}

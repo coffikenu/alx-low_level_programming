616595dc26e9a22386aa2cb7e4a98806c7c633f7
coffikenu <kenisatetemke@gmail.com>
2023-06-27 05:06:34 -0700

    yes

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcpy - check the code
+ *@dest: is first parametr
+ *@src: is second parametr
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}

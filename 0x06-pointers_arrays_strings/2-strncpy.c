ee7b300580d8e7b3a1cda714476613f2146abc2b
coffikenu <kenisatetemke@gmail.com>
2023-06-30 10:46:17 -0700

    show me

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncpy - check the code
+ * @dest: first parametr 
+ * @src: second parametr
+ * @n: third parametr
+ * Return: Always 0.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+dest = strncpy(dest, src, n);
+return (dest);
+}

da621382d3cc9605c9d6c410f4d31bf748237f27
coffikenu <kenisatetemke@gmail.com>
2023-06-30 10:59:13 -0700

    KE

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcmp - check the code
+ * @s1: first prmetr
+ * @s2: second parmetr
+ * Return: Always 0.
+ */
+int n;
+int _strcmp(char *s1, char *s2)
+{
+n = strcmp(s1, s2);
+return (n);
+}

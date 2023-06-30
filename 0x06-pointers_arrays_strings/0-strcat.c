11ad20b625c3e1ad0c4d104d162683b2f5db397e
coffikenu <kenisatetemke@gmail.com>
2023-06-30 08:46:55 -0700

    i made it

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <string.h>
+/**
+ * _strcat - check the code
+ * @dest: first parametr
+ * @src: second parametr
+ * Return: Always 0.
+ */
 char *_strcat(char *dest, char *src)
 {
 dest = strcat(dest, src);

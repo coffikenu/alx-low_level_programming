9e475cf711d235f84d7dde38490350ceaad58052
coffikenu <kenisatetemke@gmail.com>
2023-07-04 06:38:45 -0700

    last chance

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - check the code
+ * @s: first parametr
+ * @accept: second parametr
+ * Return: Always 0.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept));
+}

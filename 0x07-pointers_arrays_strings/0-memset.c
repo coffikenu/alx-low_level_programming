caceab99bc9747703b9cd730e02dddc885a8de5e
coffikenu <kenisatetemke@gmail.com>
2023-07-04 04:46:58 -0700

    yes bech

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - check the code
+ * @s: first parametr
+ * @b: second parametr
+ * @n: third parametr
+ * Return: Always 0.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+return (memset(s, b, n));
+}

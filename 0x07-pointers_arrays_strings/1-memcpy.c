307e1ef460f3a6cb9e58791974a58d78145eb8ce
coffikenu <kenisatetemke@gmail.com>
2023-07-04 05:01:59 -0700

    smoke add chill

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memcpy - check the code
+ * @dest: first parametr
+ * @src: second parametr
+ * @n: third parametr
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memcpy(dest, src, n));
+}

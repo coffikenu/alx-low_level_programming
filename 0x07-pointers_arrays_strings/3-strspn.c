c2787605e87259d5f05f6a93ad0e79cd9faa18a1
coffikenu <kenisatetemke@gmail.com>
2023-07-04 05:28:49 -0700

    3 project

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - check the code
+ * @s: first parametr
+ * @accept: second parametr
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+return (strspn(s, accept));
+}

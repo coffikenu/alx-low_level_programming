d1c5e1e2a49df65344cc42d50cafc69b5cedff60
coffikenu <kenisatetemke@gmail.com>
2023-07-04 05:14:19 -0700

    yes

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - check the code
+ * @s: first parametr
+ * @c: second parametr
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+return (strchr(s, c));
+}

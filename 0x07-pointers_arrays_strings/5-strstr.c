d0ddc3c690029dde8cd4b9f100b37ba1eb1f5044
coffikenu <kenisatetemke@gmail.com>
2023-07-04 06:45:06 -0700

    lost my way

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strstr - check the code
+ * @haystack: first pararametr
+ * @needle: second parametr
+ * Return: Always 0.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}

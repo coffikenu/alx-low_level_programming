c286c3a96e1989dfe4f49a3f8555f4e0bcb10d2c
coffikenu <kenisatetemke@gmail.com>
2023-07-07 04:40:23 -0700

    i bleve in god

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+/**
+ * main - is main method
+ * @argc: is parametr
+ * @argv: is second parametr
+ * return - Always return 0
+ */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+printf("%s\n", argv[argc - 1]);
 return (0);
 }

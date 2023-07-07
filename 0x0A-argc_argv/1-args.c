d16cc5f9ca295d177b93454fb2b11d39102bfcef
coffikenu <kenisatetemke@gmail.com>
2023-07-07 06:41:11 -0700

    i lost my way

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,13 +2,7 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-if ((sizeof(*argv) / sizeof(argv[0])) - 1 == i)
-{
-printf("%d\n", i);
-}
-}
+
+printf("%d\n\\%s", argc - 1, argv[0]);
 return (0);
 }

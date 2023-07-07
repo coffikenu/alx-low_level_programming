102d08d2e124b40874ae15e595e50c2c5bfebbcb
coffikenu <kenisatetemke@gmail.com>
2023-07-07 09:53:47 -0700

    bob marly

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,42 +1,14 @@
 #include <stdio.h>
 #include <string.h>
-int alpha(char *arg)
-{
-int i;
-int j;
-int b;
-char need[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-for (i = 0; i < strlen(arg); i++)
-{
-m = 4905
-for (j = 0; j < 52; j++)
-{
-if (arg[i] == need[j])
-{
-b = 1;
-break;
-}
-else
-{
-b = 0;
-}
-}
-if (b == 1)
-{
-break;
-}
-}
-return (b);
-}
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
 int m;
 int i;
-int c;
 if (argc  == 3)
 {
-int k = *argv[1] - '0';
-int j = *argv[2] - '0';
+int k = atoi(argv[1]);
+int j = atoi(argv[2]);
 i = k * j;
 printf("%d\n", i);
 m = 0;

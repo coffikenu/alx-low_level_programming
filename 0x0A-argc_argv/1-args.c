0cd72cbbe8e6996a42270e8ecda2294a49983dd6
coffikenu <kenisatetemke@gmail.com>
2023-07-07 07:15:01 -0700

    kenisa

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+/**
+ * main - prints buffer in hexa
+ * @argc: the address of memory to print
+ * @argv: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
 int main(int argc, char *argv[])
 {
-(void)argv;
 printf("%d\n", argc - 1);
+(void)argv;
 return (0);
 }

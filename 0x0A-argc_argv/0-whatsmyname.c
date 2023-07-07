7856d739f9e704b8a8fe5100eecc91ca540a7609
coffikenu <kenisatetemke@gmail.com>
2023-07-07 10:40:09 -0700

    fetariye erdagn

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,11 @@
  * main - is main method
  * @argc: is parametr
  * @argv: is second parametr
- * return - Always return 0
+ * Return - Always return 0
  */
 int main(int argc, char *argv[])
 {
-int i;
-for (i = 0; i < argc; i++)
-{
-printf("%s\n", argv[i]);
-}
+(void)argc;
+printf("%s\n", argv[0]);
 return (0);
 }

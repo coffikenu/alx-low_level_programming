b555af5bc450524c57394a64e58960189904790b
coffikenu <kenisatetemke@gmail.com>
2023-07-07 07:57:37 -0700

    this shit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
+int m;
 int i;
 if (argc  == 3)
 {
 int k = *argv[1] - '0';
 int j = *argv[2] - '0';
-i = j * k;
+i = k * j;
 printf("%d\n", i);
+m = 0;
 }
 else if (argc != 3)
 {
 printf("Error\n");
+m = 1;
 }
-return (0);
+return (m);
 }

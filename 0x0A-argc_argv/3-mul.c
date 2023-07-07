6cdf24121a6fd121df34e33e63e277bd6cc0f2c6
coffikenu <kenisatetemke@gmail.com>
2023-07-07 09:21:49 -0700

    lol

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,8 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+int alpha(char *arg)
+{
+int i;
+int j;
+int b;
+char need[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+for (i = 0; i < strlen(arg); i++)
+{
+m = 4905
+for (j = 0; j < 52; j++)
+{
+if (arg[i] == need[j])
+{
+b = 1;
+break;
+}
+else
+{
+b = 0;
+}
+}
+if (b == 1)
+{
+break;
+}
+}
+return (b);
+}
 int main(int argc, char *argv[])
 {
 int m;
 int i;
+int c;
 if (argc  == 3)
 {
 int k = *argv[1] - '0';

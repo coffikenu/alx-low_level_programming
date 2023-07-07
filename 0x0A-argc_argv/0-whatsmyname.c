568181fad512d628feb997cc976ac502ca384e3c
coffikenu <kenisatetemke@gmail.com>
2023-07-07 10:48:23 -0700

    lost my way

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - This is main method
- * @argc: is parametr
- * @argv: is second parametr
- * Return - Always return 0
+ *main - print the multiple of two argument
+ *@argc: first parametr
+ *@argv: second parametr
+ *Return: always zero
  */
 int main(int argc, char *argv[])
 {

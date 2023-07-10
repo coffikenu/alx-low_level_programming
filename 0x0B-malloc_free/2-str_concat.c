101a4cced524da5cb1721a9096d5cee0a2ccc44a
coffikenu <kenisatetemke@gmail.com>
2023-07-10 09:59:20 -0700

    high

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,16 +13,18 @@ char *str_concat(char *s1, char *s2)
 int len1 = s1 ? strlen(s1) : 0;
 int len2 = s2 ? strlen(s2) : 0;
 char *result = malloc(len1 + len2 + 1);
-if (!result) {
-return NULL;
+if (!result)
+{
+return (NULL);
 }
-if (len1) {
+if (len1)
+{
 memcpy(result, s1, len1);
 }
-if (len2) {
+if (len2)
+{
 memcpy(result + len1, s2, len2);
 }
 result[len1 + len2] = '\0';
-return result;
-
+return (result);
 }
